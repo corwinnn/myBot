6bc1b95b2da0c77cbe8d1c6d9cf0b2365f52f46c
korvin001 <geller_misha@mail.ru>
2018-05-12 20:14:46 +0300

    defence from incorrect input

--- bot.py
+++ bot.py
@@ -135,112 +135,47 @@ def get_message(message):
     if users[message.chat.id].status == 'words':
         user_text = message.text
         words = queries.words(user_text)
-        for word in words:
-            bot.send_message(message.chat.id, word.name)
-        users[message.chat.id].status = 'start'
+        if len(words) == 0:
+            bot.send_message(message.chat.id, 'Try again, please.')
+        else:
+            for word in words:
+                bot.send_message(message.chat.id, word.name)
+            users[message.chat.id].status = 'start'
 
     if users[message.chat.id].status == 'describe_doc':
         user_text = message.text
         files = queries.describe_doc(user_text, 'doc' + str(message.chat.id))
-        with open(files[0], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        with open(files[1], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        with open(files[2], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        users[message.chat.id].status = 'start'
+        if files[0] is not None:
+            with open(files[0], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            with open(files[1], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            with open(files[2], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            users[message.chat.id].status = 'start'
+    else:
+        bot.send_message(message.chat.id, 'Try again, please.')
+
 
     if users[message.chat.id].status == 'describe_topic':
         user_text = message.text
         files = queries.describe_topic(user_text, 'top' + str(message.chat.id))
-        bot.send_message(message.chat.id, str(files[0]) + ' articles' + '\n' + str(files[1]) +' words in the article on average')
-        with open(files[2], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        with open(files[3], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        with open(files[4], 'rb') as plot1:
-            bot.send_photo(message.chat.id, plot1)
-        users[message.chat.id].status = 'start'
+        if files[0] is not None:
+            bot.send_message(message.chat.id, str(files[0]) + ' articles' + '\n' + str(files[1]) +' words in the article on average')
+            with open(files[2], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            with open(files[3], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            with open(files[4], 'rb') as plot1:
+                bot.send_photo(message.chat.id, plot1)
+            users[message.chat.id].status = 'start'
+        else:
+            bot.send_message(message.chat.id, 'Try again, please.')
 
-
-
-
-
-
-
-
-
-
-''''@bot.message_handler(commands=['get_articles_from_title'])
-def get_articles(message):
-    setUser(message.chat.id)
-    print(message.text, message.chat.first_name, message.chat.last_name)
-    users[message.chat.id].status = 'read_number_topic'
-    bot.send_message(message.chat.id, 'Which topic?')
-
-
-@bot.message_handler(content_types=['text'])
-def repeat_message(message):
-    print(message.text, message.chat.first_name, message.chat.last_name)
-    setUser(message.chat.id)
-    if users[message.chat.id].status == 'start':
-        s = message.text[:100]
-        bot.send_message(message.chat.id, 'ok')
-    elif users[message.chat.id].status == 'read_number' or users[message.chat.id].status == 'read_number_topic' or users[message.chat.id].status == 'read_number_of_articles':
-        s = message.text
-        this_topics = ('', '', '')
-        times_articles = ''
-        try:
-            info = parser.get_titles()
-            if users[message.chat.id].topic_number != -1:
-                this_topics = Parser(info[2][users[message.chat.id].topic_number - 1]).get_titles()
-                times_articles = Parser(info[2][users[message.chat.id].topic_number - 1]).get_time()
-            if users[message.chat.id].status != 'read_number_topic' and (s == 'all' or s == 'All'):
-                s = str(len(info[0]))
-            if users[message.chat.id].status == 'read_number':
-                number = min(int(s), len(info[0]))
-            if users[message.chat.id].status == 'read_number_topic':
-                number = min(int(s), len(info[0]))
-            if users[message.chat.id].status == 'read_number_of_articles':
-                number = min(int(s), len(this_topics[0]))
-            if int(s) < 0:
-                raise ValueError
-            if users[message.chat.id].status == 'read_number':
-                for i in range(number):
-                    bot.send_message(message.chat.id, str(i+1) + '. ' + info[0][i] + '\n' + info[1][i] + '\n' + info[2][i])
-                users[message.chat.id].status = 'start'
-            elif users[message.chat.id].status == 'read_number_topic':
-                users[message.chat.id].topic_number = number
-                users[message.chat.id].status = 'read_number_of_articles'
-                bot.send_message(message.chat.id, 'How many?')
-            elif users[message.chat.id].status == 'read_number_of_articles':
-                for i in range(number):
-                    bot.send_message(message.chat.id, str(i+1) + '. ' + times_articles[i].text + '\n' + this_topics[0][i] + '\n' + this_topics[1][i] + '\n' + this_topics[2][i])
-                users[message.chat.id].articles_refs = this_topics[2]
-                bot.send_message(message.chat.id, 'Want to read something? List numbers, separated by commas')
-                users[message.chat.id].status = 'read_articles'
-        except ValueError:
-            bot.send_message(message.chat.id, 'Try again, please ')
-    elif users[message.chat.id].status == 'read_articles':
-        s = re.sub(' ', '', message.text).split(',')
-        print(s)
-        try:
-            for i in s:
-                ind = int(i)
-                if ind > len(users[message.chat.id].articles_refs):
-                    raise ValueError
-                text = ''
-                article = Parser(users[message.chat.id].articles_refs[ind]).get_paragraphs()
-                for paragraph in article:
-                    text += paragraph.text
-                bot.send_message(message.chat.id, text)
-                users[message.chat.id].status = 'start'
-
-        except ValueError:
-            bot.send_message(message.chat.id, 'Try again, please')
-
-
-'''
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except:
+            pass
--- queries.py
+++ queries.py
@@ -63,96 +63,102 @@ def words(topic):
 
 
 def describe_doc(doc_name, file_name):
-    article = Article.get(Article.name == doc_name)
-    article_words_len = json.loads(article.stat_words_len)
-    article_words_freq = json.loads(article.stat_words_freq)
-    keys = [int(x) for x in article_words_len.keys()]
-    len_max = max(keys)
-    kol_with_len = [0 for i in range(len_max + 1)]
-    for i in range(1, len_max + 1):
-        if str(i) in article_words_len.keys():
-            kol_with_len[i] = article_words_len[str(i)]
-        else:
-            kol_with_len[i] = 0
-    kol_freq = sorted(article_words_freq.values())
-    sentences = article.text.split('.')
-    sentences = sentences[:-1]
-    sent_len = [len(sent.split()) for sent in sentences]
-    file_name1 = file_name + '1.png'
-    file_name2 = file_name + '2.png'
-    file_name3 = file_name + '3.png'
-    make_plot(data=kol_with_len,
-              label="distribution of the word length",
-              xlabel='word\'s length',
-              ylabel='Number of words with this length',
-              view="bar"
-              )
-    plt.savefig(file_name1)
-    plt.close()
-    make_plot(data=kol_freq,
-              label="distribution of the different words",
-              xlabel='different words',
-              ylabel='Number of these words',
-              view="line"
-              )
+    try:
+        article = Article.get(Article.name == doc_name)
+        article_words_len = json.loads(article.stat_words_len)
+        article_words_freq = json.loads(article.stat_words_freq)
+        keys = [int(x) for x in article_words_len.keys()]
+        len_max = max(keys)
+        kol_with_len = [0 for i in range(len_max + 1)]
+        for i in range(1, len_max + 1):
+            if str(i) in article_words_len.keys():
+                kol_with_len[i] = article_words_len[str(i)]
+            else:
+                kol_with_len[i] = 0
+        kol_freq = sorted(article_words_freq.values())
+        sentences = article.text.split('.')
+        sentences = sentences[:-1]
+        sent_len = [len(sent.split()) for sent in sentences]
+        file_name1 = file_name + '1.png'
+        file_name2 = file_name + '2.png'
+        file_name3 = file_name + '3.png'
+        make_plot(data=kol_with_len,
+                  label="distribution of the word length",
+                  xlabel='word\'s length',
+                  ylabel='Number of words with this length',
+                  view="bar"
+                  )
+        plt.savefig(file_name1)
+        plt.close()
+        make_plot(data=kol_freq,
+                  label="distribution of the different words",
+                  xlabel='different words',
+                  ylabel='Number of these words',
+                  view="line"
+                  )
 
-    plt.savefig(file_name2)
-    plt.close()
-    make_plot(data=sent_len,
-              label="distribution of the number words in sentence",
-              xlabel='sentences',
-              ylabel='Number of words',
-              view="line"
-              )
+        plt.savefig(file_name2)
+        plt.close()
+        make_plot(data=sent_len,
+                  label="distribution of the number words in sentence",
+                  xlabel='sentences',
+                  ylabel='Number of words',
+                  view="line"
+                  )
 
-    plt.savefig(file_name3)
-    plt.close()
-    return file_name1, file_name2, file_name3
+        plt.savefig(file_name3)
+        plt.close()
+        return file_name1, file_name2, file_name3
+    except:
+        return None, None, None
 
 
 def describe_topic(topic_name, file_name):
-    topic = Topic.get(Topic.name == topic_name)
-    topic_words_len = json.loads(topic.stat_words_len)
-    topic_words_freq = json.loads(topic.stat_words_freq)
-    articles_amount= len(Article.select().where(Article.topic == topic.name))
-    articles_len = [len(art.text.split()) for art in Article.select().where(Article.topic == topic.name)]
-    aver = sum(articles_len)/len(articles_len)
-    keys = [int(x) for x in topic_words_len.keys()]
-    len_max = max(keys)
-    kol_with_len = [0 for i in range(len_max + 1)]
-    for i in range(1, len_max + 1):
-        if str(i) in topic_words_len.keys():
-            kol_with_len[i] = topic_words_len[str(i)]
-        else:
-            kol_with_len[i] = 0
-    kol_freq = sorted(topic_words_freq.values())
-    file_name1 = file_name + '1.png'
-    file_name2 = file_name + '2.png'
-    file_name3 = file_name + '3.png'
-    make_plot(data=kol_with_len,
-              label="distribution of the word length",
-              xlabel='word\'s length',
-              ylabel='Number of words with this length',
-              view="bar"
-              )
-    plt.savefig(file_name1)
-    plt.close()
-    make_plot(data=kol_freq,
-              label="distribution of the different words",
-              xlabel='different words',
-              ylabel='Number of these words',
-              view="line"
-              )
+    try:
+        topic = Topic.get(Topic.name == topic_name)
+        topic_words_len = json.loads(topic.stat_words_len)
+        topic_words_freq = json.loads(topic.stat_words_freq)
+        articles_amount= len(Article.select().where(Article.topic == topic.name))
+        articles_len = [len(art.text.split()) for art in Article.select().where(Article.topic == topic.name)]
+        aver = sum(articles_len)/len(articles_len)
+        keys = [int(x) for x in topic_words_len.keys()]
+        len_max = max(keys)
+        kol_with_len = [0 for i in range(len_max + 1)]
+        for i in range(1, len_max + 1):
+            if str(i) in topic_words_len.keys():
+                kol_with_len[i] = topic_words_len[str(i)]
+            else:
+                kol_with_len[i] = 0
+        kol_freq = sorted(topic_words_freq.values())
+        file_name1 = file_name + '1.png'
+        file_name2 = file_name + '2.png'
+        file_name3 = file_name + '3.png'
+        make_plot(data=kol_with_len,
+                  label="distribution of the word length",
+                  xlabel='word\'s length',
+                  ylabel='Number of words with this length',
+                  view="bar"
+                  )
+        plt.savefig(file_name1)
+        plt.close()
+        make_plot(data=kol_freq,
+                  label="distribution of the different words",
+                  xlabel='different words',
+                  ylabel='Number of these words',
+                  view="line"
+                  )
 
-    plt.savefig(file_name2)
-    plt.close()
-    make_plot(data=articles_len,
-              label="distribution of the number words in articles",
-              xlabel='Articles',
-              ylabel='Number of words',
-              view="line"
-              )
+        plt.savefig(file_name2)
+        plt.close()
+        make_plot(data=articles_len,
+                  label="distribution of the number words in articles",
+                  xlabel='Articles',
+                  ylabel='Number of words',
+                  view="line"
+                  )
 
-    plt.savefig(file_name3)
-    plt.close()
-    return articles_amount, aver, file_name1, file_name2, file_name3
+        plt.savefig(file_name3)
+        plt.close()
+        return articles_amount, aver, file_name1, file_name2, file_name3
+    except:
+        return None, None, None, None, None
