963467da0167a5ecb3441d08a9f6d89a91fac4cf
korvin001 <geller_misha@mail.ru>
2018-05-20 22:08:42 +0300

    fixed graph legend

--- queries.py
+++ queries.py
@@ -130,6 +130,7 @@ def make_plots(file_name, data_kol, data_freq, data_part, part_type):
               view="bar"
               )
     plt.savefig(file_name1)
+    plt.clf()
     plt.close()
     freq = defaultdict(int)
     for value in data_freq:
@@ -143,6 +144,7 @@ def make_plots(file_name, data_kol, data_freq, data_part, part_type):
               )
 
     plt.savefig(file_name2)
+    plt.clf()
     plt.close()
     make_plot(data=data_part,
               label="distribution of the number words in " + part_type,
@@ -152,6 +154,7 @@ def make_plots(file_name, data_kol, data_freq, data_part, part_type):
               )
 
     plt.savefig(file_name3)
+    plt.clf()
     plt.close()
     return file_name1, file_name2, file_name3
 
